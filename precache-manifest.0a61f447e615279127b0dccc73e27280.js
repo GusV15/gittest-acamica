5e83d6118d9f437040df6c2654c6c0b0fff56966
Gustavo Velasquez <gvelasquez@consultoriaglobal.com.ar>
2020-07-30 22:01:42 -0300

    Updates

---
+++ precache-manifest.0a61f447e615279127b0dccc73e27280.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c7e05b8f7123b2b31208a51e6eaca6f",
+    "url": "/gittest-acamica/index.html"
+  },
+  {
+    "revision": "f16916810fb41144c3e7",
+    "url": "/gittest-acamica/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "d1b5aca372a23aa3c375",
+    "url": "/gittest-acamica/static/js/2.89af4f3c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/gittest-acamica/static/js/2.89af4f3c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f16916810fb41144c3e7",
+    "url": "/gittest-acamica/static/js/main.68f46d70.chunk.js"
+  },
+  {
+    "revision": "89ba43b15ef46e31ddd5",
+    "url": "/gittest-acamica/static/js/runtime-main.6f68763d.js"
+  }
+]);
